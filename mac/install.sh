702559b8d4273112743bb2d289ba76042ae47386
adrien <adrien@192-168-1-104.tpgi.com.au>
2021-04-10 00:30:37 +1000

    update

--- mac/install.sh
+++ mac/install.sh
@@ -1,48 +1,43 @@
 #!/bin/bash
 
-brew cask install \
-    firefox \
-    google-chrome \
-    tunnelblick \
-    virtualbox \
-    docker \
-    iterm2 \
-    visual-studio-code \
-    intellij-idea \
-    steam \
-    spotify \
-    slack \
-    whatsapp \
-    zoomus \
-    spectacle \
-    miro-formerly-realtimeboard \
-    syncthing
+brew install --cask google-chrome 
+brew install --cask firefox 
+brew install --cask tunnelblick 
+brew install --cask virtualbox 
+brew install --cask docker
+brew install --cask iterm2 
+brew install --cask visual-studio-code 
+brew install --cask intellij-idea 
+brew install --cask steam 
+brew install --cask spotify 
+brew install --cask slack 
+brew install --cask whatsapp 
+brew install --cask spectacle 
+brew install --cask miro 
+brew install --cask syncthing
 
 brew tap cjbassi/ytop
 
-brew install \
-    node \
-    python \
-    npm \
-    vim \
-    oath-toolkit \
-    fzf \
-    bat \ #fancy cat
-    ccat \ #color cat
-    ripgrep \
-    the_silver_searcher \
-    fd \
-    ytop \ #Super fast top
-    glances \ #best htop
-    coreutils \ #The GNU Core Utilities are the basic file, shell and text manipulation utilities
-    nnn \
-    ranger \
-    lazygit \
-    lazydocker \
-    ncdu \
-    z \
-    gpg \
-    git-crypt
+brew install antigen
+curl -L git.io/antigen > ~/.antigen/antigen.zsh
+brew install node
+brew install python
+brew install npm
+brew install vim
+brew install oath-toolkit
+brew install ripgrep
+brew install the_silver_searcher
+brew install fd
+brew install glances
+brew install coreutils
+brew install gitui
+brew install lazygit
+brew install lazydocker
+brew install ncdu
+brew install z
+brew install gpg
+brew install fzf
+brew install git-crypt
 
 npm install -g \
     tldr
