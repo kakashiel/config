2a3bad5c4201965723bf6d31ebeb88e407a87239
Adrien L <adrien.loustaunau@gmail.com>
2021-09-04 12:31:06 +0200

    update myconf

--- zsh/adrien.sh
+++ zsh/adrien.sh
@@ -34,11 +34,15 @@ alias vpn='osascript -e "tell application \"/Applications/Tunnelblick.app\"" -e 
 alias top="glances"
 
 alias home_vpn=""
-alias home_rig1="ssh -i $MYPERSO/credentials/ssh/mining ad@192.168.0.51"
+alias home_rig1="ssh -i $MYPERSO/credentials/euryale/ssh/mining ad@192.168.0.51"
 
 
 #Icloud nosync node_modules
 alias nosync="find ~/Documents -name node_modules -prune -exec touch {}/.nosync \;"
+
+alias gbc="git --no-pager branch -vr --sort=committerdate"
+
+eval $(thefuck --alias)
 
 
 ###############
