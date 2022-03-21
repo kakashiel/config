1d819f9f8a4f28e36413d3b91f62fda63610b64c
Adrien L <adrien.loustaunau@gmail.com>
2022-03-21 14:46:27 +1100

    update myconf

--- init/mac/spicetify.sh
+++ init/mac/spicetify.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
+# brew install khanhas/tap/spicetify-cli
 curl -fsSL https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.sh | sh
 curl -fsSL https://raw.githubusercontent.com/CharlieS1104/spicetify-marketplace/main/install.sh | sh
-source $ZDOTDIR
+
 spicetify config extensions autoSkipVideo.js && \
 spicetify config extensions keyboardShortcut.js && \
 spicetify config extensions popupLyrics.js && \
