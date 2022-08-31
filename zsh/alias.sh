c9a171e526bd5fd02d5edc83145d572de6a69e0e
Adrien L <adrien.loustaunau@gmail.com>
2022-08-31 10:39:22 +0200

    [AL] Auto push conf

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -12,6 +12,7 @@ alias dc="docker-compose"
 alias gpmyconf="cd $MYCONF && g aa && g cam '[AL] Auto push conf' && g l && g p && cd -"
 alias gpleet="cd $MYPERSO/leetcode && g aa && g cam '[AL] Auto push leetcode' && g l && g p && cd -"
 alias gbc="git --no-pager branch -vr --sort=committerdate"
+alias groot="cd $(git rev-parse --show-toplevel)"
 
 alias sec="$MYPERSO/.sec"
 alias envv=". $MYPERSO/work/env.sh"
