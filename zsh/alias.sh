58f401e671235a9721a58c43b6cbd3ab078307cc
Adrien L <adrien.loustaunau@gmail.com>
2022-12-28 12:33:39 +1100

    [AL] Auto push conf

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -16,23 +16,14 @@ alias groot='cd $(git rev-parse --show-toplevel)'
 alias tfmt="groot; t fmt  -recursive -write=true; cd -"
 
 alias sec="$MYPERSO/.sec"
-alias envv=". $MYPERSO/work/env.sh"
-alias vpn='osascript -e "tell application \"/Applications/Tunnelblick.app\"" -e "connect \"client\"" -e "end tell" ; sec'
 
-alias note="v $MYPERSO/note.txt"
-alias todo="v $MYPERSO/todo.txt"
-alias top="glances"
+alias note="v $MYPERSO/writting/note.md"
+alias todo="v $MYPERSO/writting/todo.md"
+alias diary="v $MYPERSO/writting/diary.md"
 
 alias home_vpn="ssh pi@192.168.0.50"
 alias home_rig1="ssh -i $MYPERSO/credentials/euryale/ssh/mining ad@192.168.0.51"
-alias chrome="open -a 'Google Chrome'"
 
-
-#Icloud nosync node_modules
-alias nosync="find ~/Documents -name node_modules -prune -exec touch {}/.nosync \;"
-
-#Gammarly
-alias gram="npm >> /tmp/trash || v /tmp/test.md"
 
 # Colorize grep output (good for log files)
 alias grep='grep --color=auto'
@@ -42,17 +33,16 @@ alias fgrep='fgrep --color=auto'
 # Alias specifc to an OS
 case "$(uname -s)" in
 
-  Darwin)
-    # echo 'Mac OS X'
-    alias ls='ls -G'
+  Darwin) # echo 'Mac OS X'
+    alias chrome="open -a 'Google Chrome'"
+    #Icloud nosync node_modules
+    alias nosync="find ~/Documents -name node_modules -prune -exec touch {}/.nosync \;"
+    alias vpn='osascript -e "tell application \"/Applications/Tunnelblick.app\"" -e "connect \"client\"" -e "end tell" ; sec'
     ;;
-
   Linux)
     alias ls='ls --color=auto'
     ;;
-
-  CYGWIN* | MINGW32* | MSYS* | MINGW*)
-    # echo 'MS Windows'
+  CYGWIN* | MINGW32* | MSYS* | MINGW*)# echo 'MS Windows'
     ;;
   *)
     # echo 'Other OS'
