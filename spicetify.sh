621d93cc4ce81cb596ac1b4df453eaadcbe17e01
Adrien L <adrien.loustaunau@gmail.com>
2023-07-08 17:18:23 +0200

    [AL] Auto push conf

--- spicetify.sh
+++ spicetify.sh
@@ -1,12 +1,10 @@
 #! /bin/sh
 # brew install khanhas/tap/spicetify-cli
-curl -fsSL https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.sh | sh
-curl -fsSL https://raw.githubusercontent.com/CharlieS1104/spicetify-marketplace/main/install.sh | sh
+# curl -fsSL https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.sh | sh
+# curl -fsSL https://raw.githubusercontent.com/CharlieS1104/spicetify-marketplace/main/install.sh | sh
 
-spicetify config extensions autoSkipVideo.js && \
-spicetify config extensions keyboardShortcut.js && \
-spicetify config extensions popupLyrics.js && \
-spicetify backup apply && \
-spicetify apply
-
-
+# spicetify config extensions autoSkipVideo.js && \
+spicetify config extensions keyboardShortcut.js &&
+	spicetify config extensions popupLyrics.js &&
+	spicetify backup apply &&
+	spicetify apply
--- zsh/config/light-and-dark.sh
+++ zsh/config/light-and-dark.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export TERM_THEME="dark"
-
 lightMode() {
 	tmux source-file $MYCONF/tmux/tmux-light.conf
 	kitty +kitten themes --reload-in=all Kaolin Breeze
@@ -15,11 +13,24 @@ darkMode() {
 	export TERM_THEME="dark"
 }
 
+# macToggle() {
+# 	theme=$(defaults read -g AppleInterfaceStyle 2>/dev/null)
+# 	if [["$TERM_THEME" != "dark"]] && [[ "$theme" == "Dark" ]]; then
+# 	# if [["$TERM_THEME" != "dark"]] && [[ "$theme" == "Dark" ]]; then
+# 		darkMode
+# 	else
+# 		lightMode
+# 	fi
+#
+# }
+
+# TODO Source-file tmux-dark or light is slow find a solution to
+# reload it only one time
 macToggle() {
 	theme=$(defaults read -g AppleInterfaceStyle 2>/dev/null)
 	if [[ "$theme" == "Dark" ]]; then
 		darkMode
-	else
+	elif [[ "$theme" == "" ]]; then
 		lightMode
 	fi
 
