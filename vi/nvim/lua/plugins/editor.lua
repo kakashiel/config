19d0c1e52680eef0c037d4ab4264074d87b94a54
Adrien L <adrien.loustaunau@gmail.com>
2023-09-30 18:31:53 +0200

    [AL] Auto push conf

---
+++ vi/nvim/lua/plugins/editor.lua
@@ -0,0 +1,37 @@
+return {
+  -- which-key helps you remember key bindings by showing a popup
+  -- with the active keybindings of the command you started typing.
+  -- https://github.com/LazyVim/LazyVim/blob/6f9adbd4fba4132bd4f12404bd2b90c4a28ff136/lua/lazyvim/plugins/editor.lua#L289
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      plugins = { spelling = true },
+      defaults = {
+        mode = { "n", "v" },
+        ["g"] = { name = "+goto" },
+        ["gz"] = { name = "+surround" },
+        ["]"] = { name = "+next" },
+        ["["] = { name = "+prev" },
+        ["<leader><tab>"] = { name = "+tabs" },
+        ["<leader>b"] = { name = "+buffer" },
+        ["<leader>c"] = { name = "+code" },
+        ["<leader>f"] = { name = "+file/find" },
+        ["<leader>g"] = { name = "+git" },
+        ["<leader>gh"] = { name = "+hunks" },
+        ["<leader>r"] = { name = "+reformat" },
+        ["<leader>t"] = { name = "+gpt" },
+        ["<leader>q"] = { name = "+quit/session" },
+        ["<leader>s"] = { name = "+search" },
+        ["<leader>u"] = { name = "+ui" },
+        ["<leader>w"] = { name = "+windows" },
+        ["<leader>x"] = { name = "+diagnostics/quickfix" },
+      },
+    },
+    config = function(_, opts)
+      local wk = require("which-key")
+      wk.setup(opts)
+      wk.register(opts.defaults)
+    end,
+  },
+}
