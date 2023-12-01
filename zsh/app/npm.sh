22b4f6d3e4c93a2e491c55846c1af60cc2d32b22
Adrien L <adrien.loustaunau@gmail.com>
2023-12-01 15:39:13 +0100

    [AL] Auto push conf

--- zsh/app/npm.sh
+++ zsh/app/npm.sh
@@ -14,32 +14,33 @@ alias npmtf="npm run test:functional"
 alias yarni="yarn install"
 
 lazynvm() {
-  source $MYCONF/zsh/plugins-copy/npm.sh
-  unset -f nvm node npm npx 2>/dev/null
-  export NVM_DIR=~/.nvm
-  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-  if [ -f "$NVM_DIR/bash_completion" ]; then
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-  fi
+	source $MYCONF/zsh/plugins-copy/npm.sh
+	unset -f nvm node npm npx 2>/dev/null
+	export NVM_DIR=~/.nvm
+	[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+	if [ -f "$NVM_DIR/bash_completion" ]; then
+		[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+	fi
 }
 
 nvm() {
-  lazynvm 
-  nvm $@
+	lazynvm
+	nvm $@
 }
 
 node() {
-  lazynvm
-  node $@
+	lazynvm
+	node $@
 }
 
 npm() {
-  lazynvm
-  npm $@
+	lazynvm
+	npm $@
 }
 
 npx() {
-  lazynvm
-  npx $@
+	lazynvm
+	npx $@
 }
+
 
