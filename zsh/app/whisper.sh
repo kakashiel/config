dfd540c0fe708f12230fa2453b12ef5a15e3e4a9
Adrien L <adrien.loustaunau@gmail.com>
2024-05-12 19:07:19 +0200

    [AL] Auto push conf

--- zsh/app/whisper.sh
+++ zsh/app/whisper.sh
@@ -7,3 +7,5 @@
 
 alias record_audio="rm -rf //tmp/audio.mp3 ; ffmpeg -f avfoundation -i \":1\" //tmp/audio.mp3 -t 3600"
 alias transcript="record_audio ; whisper //tmp/audio.mp3 --model small; cat //tmp/audio.txt"
+alias transcript_fr="record_audio ; whisper //tmp/audio.mp3 --model small --language=fr; cat //tmp/audio.txt"
+alias transcript_en="record_audio ; whisper //tmp/audio.mp3 --model small --language=en; cat //tmp/audio.txt"
