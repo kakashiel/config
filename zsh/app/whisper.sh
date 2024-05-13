51f6659028d18f64505379b4c38e7ba695399e3d
Adrien L <adrien.loustaunau@gmail.com>
2024-05-13 13:43:26 +0200

    [AL] Auto push conf

--- zsh/app/whisper.sh
+++ zsh/app/whisper.sh
@@ -6,6 +6,14 @@
 # pip install -U openai-whisper
 
 alias record_audio="rm -rf //tmp/audio.mp3 ; ffmpeg -f avfoundation -i \":1\" //tmp/audio.mp3 -t 3600"
+alias create_files="touch //tmp/audio.txt"
 alias transcript="record_audio ; whisper //tmp/audio.mp3 --model small; cat //tmp/audio.txt"
 alias transcript_fr="record_audio ; whisper //tmp/audio.mp3 --model small --language=fr; cat //tmp/audio.txt"
-alias transcript_en="record_audio ; whisper //tmp/audio.mp3 --model small --language=en; cat //tmp/audio.txt"
+# tiny > base > small > large
+alias transcript_en='
+  record_audio 
+  cd //tmp/
+  whisper //tmp/audio.mp3 --model base.en --language=en;
+  cat audio.txt
+  cd -
+  '
