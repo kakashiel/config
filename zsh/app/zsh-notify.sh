ef0a4c6f1bbdf3081b5fba504fd0fc72eb4f896c
Adrien L <adrien.loustaunau@gmail.com>
2024-08-07 11:10:52 +0200

    [AL] Auto push conf

--- zsh/app/zsh-notify.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-# Only work with iterm and terminalmarzocchi/zsh-notify
-zsh_add_plugin "marzocchi/zsh-notify"
-
-source $MYZDOTDIR/plugins/zsh-notify/notify.plugin.zsh
-
-zstyle ':notify:*' command-complete-timeout 5
-zstyle ':notify:*' always-notify-on-failure no
-zstyle ':notify:*' check-focus yes
---
+++ zsh/config/notify-if-slow.sh
@@ -0,0 +1,35 @@
+# Check if the operating system is macOS
+if [[ "$(uname)" == "Darwin" ]]; then
+  # Function to check if terminal-notifier is installed, and install it if not
+  function ensure_terminal_notifier() {
+    if ! command -v terminal-notifier &> /dev/null; then
+      echo "terminal-notifier not found, installing with Homebrew..."
+      brew install terminal-notifier
+    fi
+  }
+
+  # Ensure terminal-notifier is installed
+  ensure_terminal_notifier
+
+  # Use preexec and precmd hooks to wrap every command
+  preexec() {
+    start_time=$(date +%s)
+  }
+
+  precmd() {
+    # Ensure start_time and end_time are not empty
+    if [[ -n "$start_time" ]]; then
+      end_time=$(date +%s)
+      duration=$((end_time - start_time))
+      
+      if [ $duration -gt 10 ]; then
+        echo 'Command done!' | terminal-notifier -sound default
+      fi
+      
+      # Reset start_time and end_time to avoid incorrect calculations
+      unset start_time
+      unset end_time
+    fi
+  }
+fi
+
