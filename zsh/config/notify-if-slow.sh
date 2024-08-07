4c8445dc800afe8103230e0b7df0da2883a75e18
Adrien L <adrien.loustaunau@gmail.com>
2024-08-07 11:34:09 +0200

    [AL] Auto push conf

--- zsh/config/notify-if-slow.sh
+++ zsh/config/notify-if-slow.sh
@@ -1,35 +1,34 @@
 # Check if the operating system is macOS
 if [[ "$(uname)" == "Darwin" ]]; then
-  # Function to check if terminal-notifier is installed, and install it if not
-  function ensure_terminal_notifier() {
-    if ! command -v terminal-notifier &> /dev/null; then
-      echo "terminal-notifier not found, installing with Homebrew..."
-      brew install terminal-notifier
-    fi
-  }
+	# Function to check if terminal-notifier is installed, and install it if not
+	function ensure_terminal_notifier() {
+		if ! command -v terminal-notifier &>/dev/null; then
+			echo "terminal-notifier not found, installing with Homebrew..."
+			brew install terminal-notifier
+		fi
+	}
 
-  # Ensure terminal-notifier is installed
-  ensure_terminal_notifier
+	# Ensure terminal-notifier is installed
+	ensure_terminal_notifier
 
-  # Use preexec and precmd hooks to wrap every command
-  preexec() {
-    start_time=$(date +%s)
-  }
+	# Use preexec and precmd hooks to wrap every command
+	preexec() {
+		start_time=$(date +%s)
+	}
 
-  precmd() {
-    # Ensure start_time and end_time are not empty
-    if [[ -n "$start_time" ]]; then
-      end_time=$(date +%s)
-      duration=$((end_time - start_time))
-      
-      if [ $duration -gt 10 ]; then
-        echo 'Command done!' | terminal-notifier -sound default
-      fi
-      
-      # Reset start_time and end_time to avoid incorrect calculations
-      unset start_time
-      unset end_time
-    fi
-  }
+	precmd() {
+		# Ensure start_time and end_time are not empty
+		if [[ -n "$start_time" ]]; then
+			end_time=$(date +%s)
+			duration=$((end_time - start_time))
+
+			if [ $duration -gt 5 ]; then
+				echo 'Command done!' | terminal-notifier -sound default
+			fi
+
+			# Reset start_time and end_time to avoid incorrect calculations
+			unset start_time
+			unset end_time
+		fi
+	}
 fi
-
